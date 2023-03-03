4e5912c6a9c47b607c89d52f6900d08e33987f28
simplon-ThomasJulian <107990221+simplon-ThomasJulian@users.noreply.github.com>
2023-03-03 13:24:11 +0100

    Update auto_maj.sh
    change auto maj

--- auto_maj.sh
+++ auto_maj.sh
@@ -1,5 +1,3 @@
-
-
 #!/usr/bin/env bash
 # coding: utf-8
 # vi: tabstop=8 expandtab shiftwidth=4 softtabstop=4
@@ -8,10 +6,8 @@ set -e
 
 PATCH=$(ag --nonumbers '^ver =' azure-vote/main.py | sed -re 's/ver = "1\.0\.([0-9]+)"/\1/')
 
-while true; do
-    PATCH=$((PATCH+1))
-    sed -i -re "s/(ver = \"1\.0\.).(.*)$/\1${PATCH}\2/" azure-vote/main.py
-    git commit -a -m "new vers" && git push
-done
+PATCH=$((PATCH+1))
+sed -i -re "s/(ver = \"1\.0\.).(.*)$/\1${PATCH}\2/" azure-vote/main.py
+git commit -a -m "new vers" && git push
 
 exit 0
