0a739194f4438ae584e06295a7cbe4d797d7c692
simplon-ThomasJulian <th.julian23@gmail.com>
2023-03-30 15:21:21 +0200

    suppression de ce qui sert à rien

--- test-reponse/test-reponse-prod.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/
-curl -w "@curl-format.txt" -o /dev/null -iF 'vote=free' -s "http://votingappkube.simplon-thomas.space"
--- test-reponse/test-reponse-qal.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/
-curl -w "@curl-format.txt" -o /dev/null -iF 'vote=free' -s "http://qal.simplon-thomas.space"
